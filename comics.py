8b5071f2c66dc52b34d42de558ddc8a07dd5ed92
pyt0710 <69299330+pyt0710@users.noreply.github.com>
2020-09-08 18:02:19 +0800

    Update comics.py

--- comics.py
+++ comics.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
+import re,os
 
 
-browser = webdriver.ChromeOptions()
-browser.add_argument('--headless')
-html = webdriver.Chrome(options=browser)
-html.get('https://www.manhuabei.com/manhua/haizeiwang/68441.html?p=42')
 
-imgs = html.find_element_by_xpath('//div[@class="chapter-view"]//div[@id="images"]//img').get_attribute("src")
-print(imgs)
+title = ""
+def get_image():
+    browser = webdriver.ChromeOptions()
+    browser.add_argument('--headless')
+    html = webdriver.Chrome(options=browser)
+    html.get('https://www.manhuabei.com/manhua/haizeiwang/499917.html')
+
+    imgs = html.find_element_by_xpath('//div[@class="chapter-view"]//div[@id="images"]//img').get_attribute("src")
+    print(imgs)
+    title = html.find_element_by_xpath('//div[@class="chapter-view"]//div[@class="head_title"]//h2').text
+    print(title)
+    return title
+
+get_image()
+
+
+folder_name = title
+os.makedirs(folder_name)
